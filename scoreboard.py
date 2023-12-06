92e1b694db2ac5cde5f7cbb6181f763f506ed056
Matheus Oliveira <112003358+Domi-ni@users.noreply.github.com>
2023-12-06 14:15:03 -0300

    created scoreboard and update every level pass

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,31 @@
-FONT = ("Courier", 24, "normal")
+from turtle import Turtle
+FONT = ("Courier", 16, "bold")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 1
+        self.hideturtle()
+        self.color("black")
+        self.penup()
+        self.update_scoreboard()
+
+    def score_point(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(x=-235, y=260)
+        self.write(arg=f"Level: {self.score}", align="center", font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(arg="GAME OVER", align="center", font=FONT)
+
+
+
+
+
+
