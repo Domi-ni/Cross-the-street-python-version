e119e40f06630a8a37d0e0c26d2d51090c37087c
Matheus Oliveira <112003358+Domi-ni@users.noreply.github.com>
2023-12-06 12:37:26 -0300

    created turtle and make it move

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player_turtle = Player()
+
+screen.listen()
+screen.onkeypress(key="Up", fun=player_turtle.walk)
+
 game_is_on = True
 while game_is_on:
+
     time.sleep(0.1)
+    player_turtle.finish_line()
     screen.update()
